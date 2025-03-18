6c7801941888e09d53b65307830adf5339ccf32f
KyleW888 <kwucontact@gmail.com>
2025-03-18 19:56:09 -0400

    Update attackData.js

--- attackData.js
+++ attackData.js
@@ -1,5 +1,62 @@
 var attackData = [
-    { lat: 37.7749, lng: -122.4194, info: "Attack from San Francisco" },
-    { lat: 51.5074, lng: -0.1278, info: "Attack from London" },
-    { lat: 35.6895, lng: 139.6917, info: "Attack from Tokyo" }
+    {
+        lat: 54.6916,
+        lng: 25.2874,
+        info: "Attack from 45.227.254.153"
+    },
+    {
+        lat: 10.2502,
+        lng: 105.9651,
+        info: "Attack from 115.75.106.119"
+    },
+    {
+        lat: 22.3193,
+        lng: 114.169,
+        info: "Attack from 154.212.141.173"
+    },
+    {
+        lat: 48.9742,
+        lng: 8.1851,
+        info: "Attack from 161.97.72.122"
+    },
+    {
+        lat: -17.743,
+        lng: 168.317,
+        info: "Attack from 147.45.112.182"
+    },
+    {
+        lat: 41.8781,
+        lng: -87.6298,
+        info: "Attack from 45.156.129.65"
+    },
+    {
+        lat: 52.1783,
+        lng: 21.0602,
+        info: "Attack from 95.214.55.202"
+    },
+    {
+        lat: 30.0507,
+        lng: 31.2489,
+        info: "Attack from 41.38.56.85"
+    },
+    {
+        lat: 55.7558,
+        lng: 37.6173,
+        info: "Attack from 193.37.69.105"
+    },
+    {
+        lat: 54.9072,
+        lng: 24.0019,
+        info: "Attack from 141.98.10.54"
+    },
+    {
+        lat: 36.5916,
+        lng: 127.2916,
+        info: "Attack from 211.185.207.14"
+    },
+    {
+        lat: 52.2299,
+        lng: 21.0093,
+        info: "Attack from 149.50.96.98"
+    },
 ];
