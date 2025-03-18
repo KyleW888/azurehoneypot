502f6604f058f2779e9b66151848492cba3e5fd1
KyleW888 <kwucontact@gmail.com>
2025-03-18 18:52:19 -0400

    Create attackData.js

---
+++ attackData.js
@@ -0,0 +1,5 @@
+var attackData = [
+    { lat: 37.7749, lng: -122.4194, info: "Attack from San Francisco" },
+    { lat: 51.5074, lng: -0.1278, info: "Attack from London" },
+    { lat: 35.6895, lng: 139.6917, info: "Attack from Tokyo" }
+];
