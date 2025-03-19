c550e5fa31bac65784e7052b9e5d50da469a07f3
KyleW888 <kwucontact@gmail.com>
2025-03-18 20:08:51 -0400

    Update attackData.js

--- attackData.js
+++ attackData.js
@@ -1,14 +1,62 @@
 var attackData = [
-    { lat: 54.6916, lng: 25.2874, info: "Attack from Lithuania" },
-    { lat: 10.2502, lng: 105.9651, info: "Attack from Vietnam" },
-    { lat: 22.3193, lng: 114.169, info: "Attack from Hong Kong" },
-    { lat: 48.9742, lng: 8.1851, info: "Attack from France" },
-    { lat: -17.743, lng: 168.317, info: "Attack from Vanuatu" },
-    { lat: 41.8781, lng: -87.6298, info: "Attack from United States" },
-    { lat: 52.1783, lng: 21.0602, info: "Attack from Poland" },
-    { lat: 30.0507, lng: 31.2489, info: "Attack from Egypt" },
-    { lat: 55.7558, lng: 37.6173, info: "Attack from Russia" },
-    { lat: 54.9072, lng: 24.0019, info: "Attack from Lithuania" },
-    { lat: 36.5916, lng: 127.2916, info: "Attack from South Korea" },
-    { lat: 52.2299, lng: 21.0093, info: "Attack from Poland" }
+    {
+        lat: 54.6916,
+        lng: 25.2874,
+        info: "Attack from Lithuania"
+    },
+    {
+        lat: 10.2502,
+        lng: 105.9651,
+        info: "Attack from Vietnam"
+    },
+    {
+        lat: 22.3193,
+        lng: 114.169,
+        info: "Attack from Hong Kong"
+    },
+    {
+        lat: 48.9742,
+        lng: 8.1851,
+        info: "Attack from France"
+    },
+    {
+        lat: -17.743,
+        lng: 168.317,
+        info: "Attack from Vanuatu"
+    },
+    {
+        lat: 41.8781,
+        lng: -87.6298,
+        info: "Attack from United States"
+    },
+    {
+        lat: 52.1783,
+        lng: 21.0602,
+        info: "Attack from Poland"
+    },
+    {
+        lat: 30.0507,
+        lng: 31.2489,
+        info: "Attack from Egypt"
+    },
+    {
+        lat: 55.7558,
+        lng: 37.6173,
+        info: "Attack from Russia"
+    },
+    {
+        lat: 54.9072,
+        lng: 24.0019,
+        info: "Attack from Lithuania"
+    },
+    {
+        lat: 36.5916,
+        lng: 127.2916,
+        info: "Attack from South Korea"
+    },
+    {
+        lat: 52.2299,
+        lng: 21.0093,
+        info: "Attack from Poland"
+    }
 ];
