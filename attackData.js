b2694dd98c66e5424a7fb5160c627f17bd297f43
KyleW888 <kwucontact@gmail.com>
2025-03-18 20:01:44 -0400

    Update attackData.js

--- attackData.js
+++ attackData.js
@@ -2,61 +2,61 @@ var attackData = [
     {
         lat: 54.6916,
         lng: 25.2874,
-        info: "Attack from 45.227.254.153"
+        info: "Attack from Lithuania"
     },
     {
         lat: 10.2502,
         lng: 105.9651,
-        info: "Attack from 115.75.106.119"
+        info: "Attack from Vietnam"
     },
     {
         lat: 22.3193,
         lng: 114.169,
-        info: "Attack from 154.212.141.173"
+        info: "Attack from Hong Kong"
     },
     {
         lat: 48.9742,
         lng: 8.1851,
-        info: "Attack from 161.97.72.122"
+        info: "Attack from France"
     },
     {
         lat: -17.743,
         lng: 168.317,
-        info: "Attack from 147.45.112.182"
+        info: "Attack from Vanuatu"
     },
     {
         lat: 41.8781,
         lng: -87.6298,
-        info: "Attack from 45.156.129.65"
+        info: "Attack from United States"
     },
     {
         lat: 52.1783,
         lng: 21.0602,
-        info: "Attack from 95.214.55.202"
+        info: "Attack from Poland"
     },
     {
         lat: 30.0507,
         lng: 31.2489,
-        info: "Attack from 41.38.56.85"
+        info: "Attack from Egypt"
     },
     {
         lat: 55.7558,
         lng: 37.6173,
-        info: "Attack from 193.37.69.105"
+        info: "Attack from Russia"
     },
     {
         lat: 54.9072,
         lng: 24.0019,
-        info: "Attack from 141.98.10.54"
+        info: "Attack from Lithuania"
     },
     {
         lat: 36.5916,
         lng: 127.2916,
-        info: "Attack from 211.185.207.14"
+        info: "Attack from South Korea"
     },
     {
         lat: 52.2299,
         lng: 21.0093,
-        info: "Attack from 149.50.96.98"
-    },
+        info: "Attack from Poland"
+    }
 ];
